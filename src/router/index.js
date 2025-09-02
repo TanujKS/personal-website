9f3cf7a3c0832f2a50025621da7a8e24a31b108a
TanujKS <tanujsiripurapu@gmail.com>
2025-09-01 22:40:04 -0700

    add: project redirects

--- src/router/index.js
+++ src/router/index.js
@@ -4,18 +4,38 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('../components/Home.vue')
+    component: () => import('../components/Home.vue'),
   },
   {
     path: '/contact',
     name: 'Contact',
-    component: () => import('../components/Contact.vue')
-  }
+    component: () => import('../components/Contact.vue'),
+  },
+  {
+    path: '/droplet',
+    redirect: 'https://youtu.be/bDuVqdedkZA',
+  },
+  {
+    path: '/facade',
+    redirect: 'https://youtu.be/YABhQ6WrWmk?si=5u-BQxc_OwY4VEdi',
+  },
+  {
+    path: '/protected',
+    redirect: 'https://devpost.com/software/protected/',
+  },
+  {
+    path: '/writeright',
+    redirect: 'https://devpost.com/software/writeright-pq8ihr',
+  },
+  {
+    path: '/zeroresponders',
+    redirect: 'https://devpost.com/software/zeroresponders',
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory('/'),
-  routes
+  routes,
 })
 
 export default router
